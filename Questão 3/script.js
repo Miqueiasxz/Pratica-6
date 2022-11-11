e88776408fc2d9ab26f60b66b8f4085a1f7295ca
Miqueiasxz <99367419+Miqueiasxz@users.noreply.github.com>
2022-11-10 23:50:32 -0300

    Add files via upload

---
+++ Questão 3/script.js
@@ -0,0 +1,3 @@
+let arr = [1,8,11,9,2,3,5,16,6,32];
+let sort = arr.sort((a,b) => a - b);
+console.log(sort);
---
+++ Questão 4/script.js
@@ -0,0 +1,5 @@
+let arr = [2, 4, 15, 9, 12];
+let media = arr.map(function(num) {
+    return num /2;
+  });
+console.log(quad);
